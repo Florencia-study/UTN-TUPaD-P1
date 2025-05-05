bf427bff7b835bd4c6855bf93f7b92beed080089
Florencia <florenciaanabelintaschi@gmail.com>
2025-05-04 23:19:12 -0300

    CORREGIDO EJ 6

--- 04 Estructuras Repetitivas/INTASCHI_TP4_Ej6.py
+++ 04 Estructuras Repetitivas/INTASCHI_TP4_Ej6.py
@@ -1,6 +1,6 @@
 #6)imprima en pantalla todos los números pares comprendidos entre 0 y 100, en orden decreciente.
 
-for i in range (100,-1,-1):
+for i in range (100,-1,-2):
     print (i)
     i=i-1
 
