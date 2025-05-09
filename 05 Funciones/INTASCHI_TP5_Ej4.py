57212e2678583f06814e38b2dfc37b2728f3da79
Florencia <florenciaanabelintaschi@gmail.com>
2025-05-08 23:27:53 -0300

    guardar ej 4

---
+++ 05 Funciones/INTASCHI_TP5_Ej4.py
@@ -0,0 +1,14 @@
+#4
+
+animales = ["perro", "gato", "conejo", "pez"] 
+print(animales)
+
+animales[1]="loro"
+animales[3]="oso"
+
+print(animales)
+
+#con indexing negativo
+animales[-3]="loro"
+animales[-1]="oso"
+print(animales)
