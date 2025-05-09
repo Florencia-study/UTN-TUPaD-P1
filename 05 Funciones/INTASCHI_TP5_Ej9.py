bf7706611016cf4c0671d41e7c09c96ffc62f23f
Florencia <florenciaanabelintaschi@gmail.com>
2025-05-08 23:52:43 -0300

    guardar ej 9

---
+++ 05 Funciones/INTASCHI_TP5_Ej9.py
@@ -0,0 +1,18 @@
+#9
+
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]] 
+
+#a)
+compras[2].append("jugo")
+print(f"a) {compras}")
+
+#b)
+compras[1][1]="tallarines"
+print(f"b) {compras}")
+
+#c)
+compras[0].remove("pan")
+print(f"c) {compras}")
+
+#d)
+print(f"Imprimir lista resultante: {compras}")
