e5aab43b05e12918993378cdebbb2a428ac44966
Florencia <florenciaanabelintaschi@gmail.com>
2025-05-08 23:36:18 -0300

    guardar ej 6

---
+++ 05 Funciones/INTASCHI_TP5_Ej6.py
@@ -0,0 +1,8 @@
+#6
+
+lista=list(range(10,31,5))
+#prueba para verificar si la lista realmente salta de 5 en 5
+print(lista)
+
+#cumplimiento de la consigna: que muestre los dos primeros elementos de la lista.
+print(lista[0:2])
