8806409c030e69219ec17355449494fb8817a794
Florencia <florenciaanabelintaschi@gmail.com>
2025-05-08 23:15:09 -0300

    guardar ej 2

---
+++ 05 Funciones/INTASCHI_TP_Ej2.py
@@ -0,0 +1,13 @@
+#2
+
+mi_lista=["cosas","niño","programación",4,True]
+
+#mostrar penúltimo elemento usando slicing.
+print(mi_lista[3])
+
+#mostrar penúltimo elemento usando indexing con números negativos.
+print(mi_lista[-2])
+
+
+
+
